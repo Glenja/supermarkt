b194e4bbeb96baba8869118f5ca8ae8b70f6ad81
Nick Alfa <nicholas.alfa@hotmail.co.uk>
2022-03-01 18:52:44 +0000

    added name and household to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_174422) do
+ActiveRecord::Schema.define(version: 2022_03_01_184731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,10 @@ ActiveRecord::Schema.define(version: 2022_03_01_174422) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.bigint "household_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["household_id"], name: "index_users_on_household_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -73,4 +76,5 @@ ActiveRecord::Schema.define(version: 2022_03_01_174422) do
   add_foreign_key "lists", "orders"
   add_foreign_key "lists", "users"
   add_foreign_key "orders", "households"
+  add_foreign_key "users", "households"
 end
