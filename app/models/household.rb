1e1e21b403d6d2e0c8c52a62eb214150807dbcc3
Nick Alfa <nicholas.alfa@hotmail.co.uk>
2022-03-02 15:38:53 +0000

    ammended household to enable user sign up

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,6 +1,6 @@
 class Household < ApplicationRecord
   has_many :orders
   has_many :users
-  validates :address, presence: true
-  validates :name, presence: true
+  # validates :address, presence: true
+  # validates :name, presence: true
 end
