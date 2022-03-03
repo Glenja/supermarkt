7be49e8533627b914ce2c1082b199247e4f1d4af
Seb Naylor <sebnaylor@gmail.com>
2022-03-03 15:07:11 +0000

    Completed keyup functionality and ability to click on search results to add to simple form

--- app/javascript/controllers/items_controller.js
+++ app/javascript/controllers/items_controller.js
@@ -1,42 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "query" ]
+  static values = {
+    key: String
+  }
 
   connect() {
     console.log("hello from items_controller!")
   };
-  apiQuery () {
 
-    console.log(this.queryTarget.innerHTML)
-
-    console.log("querying the database")
-    fetch('https://api.spoonacular.com/food/ingredients/autocomplete?query=appl&apiKey=1613fe995a224494910194cdb1b5401f')
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-
-
-    // const searchIngredients = query => {
-    //   console.log("searching API")
-    //   //Forming the query URL
-    //   const API_KEY = '1613fe995a224494910194cdb1b5401f';
-    //   let numberResults = 6
-    //   const url = 'https://api.spoonacular.com/food/ingredients/autocomplete?'+
-    //   'apiKey='+ API_KEY +
-    //   '&query='+ query +
-    //   '&number=' + numberResults
-    //   console.log(query);
-    //   console.log(API_KEY);
-    //   console.log(numberResults);
-
-    //   //Sending request
-    //   const response = fetch(url);
-    //   const ingredientResults = response.json();//convert response to JSON
-    //   outputHTML(ingredientResults);
-    //   console.log(ingredientResults);
-    //   console.log(response);
-    // }
+  addText(event) {
+    document.getElementById('item_name').value = event.target.innerHTML
   }
-}
 
-// https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=1613fe995a224494910194cdb1b5401f&query=
+  apiQuery () {
+    console.log(this.queryTarget.value)
+    console.log("querying the database")
+    fetch(`https://api.spoonacular.com/food/ingredients/autocomplete?query=${this.queryTarget.value}&apiKey=${this.keyValue}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      results.innerHTML = ''
+      data.forEach((result, index) => {
+        const searchResult = `<li class="search-inline-item" data-action="click->items#addText">
+        <p>${result.name}</p>
+        </li>`
+        results.insertAdjacentHTML("beforeend",searchResult)
+      })
+    })
+  }
+
+}
