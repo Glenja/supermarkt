cfaf4c267837cbb846b2469d7d533a3a362235b7
Oli Finn <oliver.james.finn@gmail.com>
2022-03-07 15:42:08 +0000

    check buttons functionality and styling added to orders show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def mark_done
+    @item = Item.find(params[:id])
+    if @item.done?
+      @item.update_attribute(:done, false)
+    else
+      @item.update_attribute(:done, true)
+    end
+    redirect_to household_order_path(@item.list.order.household, @item.list.order)
+  end
+
   def update
+    @item = Item.find (params[:id])
+    @item.update(item_params)
+    @item.save!
+    redirect_to household_order_path(@item.list.order.household, @item.list.order)
   end
 
   def destroy
@@ -22,6 +36,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :quantity, :aisle, :category, :price, :list, :unit)
+    params.require(:item).permit(:name, :aisle, :list_id, :quantity, :category, :price, :unit, :done)
   end
 end
